71d7c79ef3704871e5be89ca40967e42b52d9150
JonathanJairxd <jonathanjairxd02@gmail.com>
2025-05-19 18:24:40 -0500

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
+  offset = 0;
+  limit = 20;
+  loading = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  loadPokemons(event?: any) {
+    if (this.loading) return;
+    this.loading = true;
+
+    this.http
+      .get<any>(`https://pokeapi.co/api/v2/pokemon?offset=${this.offset}&limit=${this.limit}`)
+      .subscribe((res) => {
+        this.pokemons = [...this.pokemons, ...res.results];
+        this.offset += this.limit;
+        this.loading = false;
+
+        if (event) {
+          event.target.complete();
+        }
+
+        // Disable scroll if no more Pokémon
+        if (res.next === null && event) {
+          event.target.disabled = true;
+        }
+      });
+  }
+
+  getImageUrl(index: number): string {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter} from '@angular/router';
+import {  IonicModule } from "@ionic/angular";
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideHttpClient(),
+    provideRouter(routes),
+  ],
+});
+
